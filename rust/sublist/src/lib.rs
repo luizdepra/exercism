365af6d69802cb91cbad6e112a851872c2658db2
Luiz F. A. de Prá <luiz.pra@olist.com>
2019-03-08 17:02:35 -0300

    Add rust sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,6 @@
+use rayon::iter::ParallelIterator;
+use rayon::slice::ParallelSlice;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,16 +9,23 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    let is_sublist = |a: &[T], b: &[T]| b.is_empty() || a.windows(b.len()).any(|s| s == b);
+pub fn sublist<T: PartialEq + Sync>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let first_length: usize = first_list.len();
+    let second_length: usize = second_list.len();
+    let is_first_empty: bool = first_list.is_empty();
+    let is_second_empty: bool = second_list.is_empty();
 
-    if first_list == second_list {
-        Comparison::Equal
-    } else if is_sublist(second_list, first_list) {
-        Comparison::Sublist
-    } else if is_sublist(first_list, second_list) {
-        Comparison::Superlist
-    } else {
-        Comparison::Unequal
-    }
+    let sublist_or_superlist = || -> Comparison {
+        if first_list == second_list {
+            Comparison::Equal
+        } else if is_first_empty || second_list.par_windows(first_length).any(|s| s == first_list) {
+            Comparison::Sublist
+        } else if is_second_empty || first_list.par_windows(second_length).any(|s| s == second_list) {
+            Comparison::Superlist
+        } else {
+            Comparison::Unequal
+        }
+    };
+
+    sublist_or_superlist()
 }
