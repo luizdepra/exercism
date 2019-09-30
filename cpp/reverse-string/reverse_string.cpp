4576d0578ea31180a21221b04c5710ff2f6424af
Luiz F. A. de Prá <rawarkanis@gmail.com>
2019-09-30 19:13:14 -0300

    Add cpp reverse string

---
+++ cpp/reverse-string/reverse_string.cpp
@@ -0,0 +1,11 @@
+#include "reverse_string.h"
+
+#include <algorithm>
+
+namespace reverse_string {
+    std::string reverse_string(std::string input)
+    {
+        std::reverse(input.begin(), input.end());
+        return input;
+    }
+}
