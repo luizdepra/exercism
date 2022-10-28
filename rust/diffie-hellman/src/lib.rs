f004dbd9e1fc0a49e518027cfd72bde10d56688c
Luiz F. A. de Prá <luiz.pra@luizalabs.com>
2022-10-28 17:09:34 -0300

    Update diffie-helman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,22 +1,32 @@
-use rand::Rng;
+use rand::prelude::*;
 
 pub fn private_key(p: u64) -> u64 {
-    rand::thread_rng().gen_range(2..p)
+    thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    modular_pow(p as u128, g as u128, a as u128) as u64
+    modular_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    modular_pow(p as u128, b_pub as u128, a as u128) as u64
+    modular_pow(b_pub, a, p)
 }
 
-fn modular_pow(p: u128, g: u128, a: u128) -> u128 {
-    let mut value = 1u128;
-    for _ in 0..a {
-        value = (value * g) % p;
+fn modular_pow(b: u64, e: u64, m: u64) -> u64 {
+    if m == 1 {
+        return 0;
     }
 
-    value
+    let mut result = 1;
+    let mut base = b % m;
+    let mut exp = e;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % m;
+        }
+        exp >>= 1;
+        base = base * base % m;
+    }
+
+    result
 }
